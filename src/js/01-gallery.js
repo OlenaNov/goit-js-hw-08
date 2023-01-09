2d5ced69014c3ed25feb74798b982aa72dd43308
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-01-09 13:58:15 +0100

    task1,3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const addItems = galleryItems.reduce((acc, { original, preview, description }) => 
+    acc += `<a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                title="${description}" 
+            />
+        </a>`, "");
+
+gallery.insertAdjacentHTML('beforeend', addItems);
+
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: `alt`,
+        animationSpeed:	250,
+});
